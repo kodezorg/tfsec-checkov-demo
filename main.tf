cf091239ad0bd37c0e473b3c54e0159e83c80fcb
sarasa-kodez <sarasa.gunawardana@kodez.com.au>
2022-09-30 10:40:03 +0530

    configured checkov & tfsec

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "bad_example" {
+  ami = "ami-7f89a64f"
+  instance_type = "t1.micro"
+  metadata_options {
+     http_tokens = "required"
+     }  
+  monitoring = true
+  ebs_optimized = true
+
+  root_block_device {
+      encrypted = true
+  }
+
+  ebs_block_device {
+    device_name = "/dev/sdg"
+    volume_size = 5
+    volume_type = "gp2"
+    delete_on_termination = false
+    encrypted = true
+  }
+}
